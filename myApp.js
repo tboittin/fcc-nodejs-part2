d8c63257c9decd5e40b72f520c3e2f9fbd2f2477
Thomas Boittin <53752390+tboittin@users.noreply.github.com>
2022-06-13 13:08:03 +0000

    serving html file

--- myApp.js
+++ myApp.js
@@ -3,8 +3,9 @@ let app = express();
 
 console.log("Hello World");
 
+const path = __dirname + "/views/index.html"
 app.get("/", function(req, res) {
-  res.send('Hello Express')
+  res.sendFile(path)
 })
 
 module.exports = app;
