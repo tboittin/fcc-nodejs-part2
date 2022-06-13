7cb64c2785b8f892b462a2a6bdf5ea819d848315
Thomas Boittin <53752390+tboittin@users.noreply.github.com>
2022-06-13 13:04:07 +0000

    express message

--- myApp.js
+++ myApp.js
@@ -1,39 +1,10 @@
 let express = require('express');
 let app = express();
 
-
-
-
-
-
-
-
-
-
-
-
-
 console.log("Hello World");
 
+app.get("/", function(req, res) {
+  res.send('Hello Express')
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
