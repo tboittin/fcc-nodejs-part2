bbd43bbdadcc15bd051c33f14e104948c9bde7d6
Thomas Boittin <53752390+tboittin@users.noreply.github.com>
2022-06-13 13:28:41 +0000

    add a json endpoint serving a json file

--- myApp.js
+++ myApp.js
@@ -1,14 +1,19 @@
 let express = require('express');
 let app = express();
 
-const assetPath = __dirname + "/public"
+const assetPath = __dirname + "/public";
 
-app.use("/public", express.static(assetPath))
+app.use("/public", express.static(assetPath));
 
-const filePath = __dirname + "/views/index.html"
+const filePath = __dirname + "/views/index.html";
 
 app.get("/", function(req, res) {
-  res.sendFile(filePath)
-})
+  res.sendFile(filePath);
+});
+
+const message = {"message":"Hello json"};
+app.get("/json", function(req, res) {
+  res.json(message);
+});
 
 module.exports = app;
